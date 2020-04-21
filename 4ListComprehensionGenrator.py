3d06649fda7d44968df03ba41520668fbc236f4d
amit <pundrikmishra1234@gmail.com>
2020-04-21 16:34:43 +0530

    21-04-2020

---
+++ 4ListComprehensionGenrator.py
@@ -0,0 +1,18 @@
+# xyz = [i for i in range(5)]
+# print(xyz)
+
+# xyz = []
+# for i in range(5):
+#     xyz.append(i)
+# print(xyz)
+
+
+
+xyz = [i for i in range(500000)]
+print("done")
+
+xyz = (i for i in range(50000000000000000000000000000000000000))
+print(xyz)
+
+# for i in xyz:
+#     print(i)
