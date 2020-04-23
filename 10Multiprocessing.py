004634566f9b04fb6b03774fe1f87a0b27d60dd0
amit <pundrikmishra1234@gmail.com>
2020-04-23 16:22:16 +0530

    22-02-2020

--- 10Multiprocessing.py
+++ 10Multiprocessing.py
@@ -4,7 +4,7 @@ def spawn(num, num2):
     print('Pundrik!{} {}'. format(num, num2))
 
 if __name__ == '__main__':
-    for i in range(100000):
+    for i in range(55):
         p = multiprocessing.Process(target=spawn, args=(i,i+1))
         p.start()
         p.join()
